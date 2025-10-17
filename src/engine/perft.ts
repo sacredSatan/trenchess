9f367a95d90fd9f22876d7322a86358e6fc6daf2
sacredSatan <iamniharpatel@gmail.com>
2025-10-18 03:20:46 +0530

    fix perft by ignoring illegal moves

--- src/engine/perft.ts
+++ src/engine/perft.ts
@@ -1,25 +1,37 @@
 import Engine from "./index";
 
 // const _engine = new Engine();
-const sleep = () => new Promise((resolve) => setTimeout(resolve, 0));
+const sleep = () => new Promise((resolve) => setTimeout(resolve, 500));
 const moveSet = new Set();
 export const perft = async (depth: number, _engine?: Engine, callback?: any) => {
+  if (depth === 0) {
+    return 1;
+  }
+
   const engine = _engine || new Engine();
   const moves = Array.from(engine.generateLegalMoves()) as string[];
   // console.log(moves);
   const nMoves = moves.length;
-  if (depth === 1) {
-    return nMoves;
-  }
+
+  // if(depth === 1) {
+  //   const validMoves = moves.filter((move) => {
+  //     const res = engine.move(move, { skipCommit: true });
+  //     return res > 1;
+  //   });
+
+  //   return validMoves.length;
+  // }
 
   let nodes = 0;
   for (let i = 0; i < nMoves; i++) {
     // console.log("perft:: depth: ", depth, "move: ", i+1);
     const move = moves[i];
     const res = engine.move(move);
-    // if(move === "d8 h4") {
-    //   console.log("perft::queen checkmate?");
-    // }
+    if(res < 2) {
+      // console.log("illegal!! ", move);
+      // engine.draw();
+      continue;
+    }
     moveSet.add(res);
     if(callback) {
       callback();
@@ -36,23 +48,23 @@ export const perft = async (depth: number, _engine?: Engine, callback?: any) => 
   return nodes;
 }
 
-// const depth = 5;
-// const results = {
-//   1: 20,
-//   2: 400,
-//   3: 8902,
-//   4: 197281,
-//   5: 4865609,
-// } as any;
+const depth = 5;
+const results = {
+  1: 20,
+  2: 400,
+  3: 8902,
+  4: 197281,
+  5: 4865609,
+} as any;
 
-// for(let i = 1; i <= depth; i++) {
-//   const t0 = performance.now();
-//   console.log(Array.from(moveSet));
-//   const perftRes = await perft(i, undefined, null);
-//   console.log(i, "found: ", perftRes, "expected: ", results[i], "match: ", perftRes === results[i]);
-//   console.log(Array.from(moveSet));
-//   moveSet.clear();
-//   const t1 = performance.now();
-//   console.log("timespent: ", t1 - t0);
-// }
+for(let i = 1; i <= depth; i++) {
+  const t0 = performance.now();
+  console.log(Array.from(moveSet));
+  const perftRes = await perft(i, undefined, null);
+  console.log(i, "found: ", perftRes, "expected: ", results[i], "match: ", perftRes === results[i]);
+  console.log(Array.from(moveSet));
+  moveSet.clear();
+  const t1 = performance.now();
+  console.log("timespent: ", t1 - t0);
+}
 
